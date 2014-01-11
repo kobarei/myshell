1dd660223ff5d1d54f891f04346c49b48d9197c9
kobarei <koba.reiji@gmail.com>
2014-01-11 23:38:46 +0900

    refs #1 become to exec command

--- mysh.c
+++ mysh.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -11,30 +12,45 @@
 #define BUF_SIZE 256
 
 bool prom = true;
+struct stat info;
+bool cmd_aru = false;
 
 bool is_exist(char *path_cmd);
-char *read_path(char *cmd);
+char *build_path(char *cmd);
 
 int main(int argc, char const *argv[]) {
-  char cmd[BUF_SIZE];
+  static char cmd[BUF_SIZE];
+  char path_cmd[BUF_SIZE];
+  char *args[BUF_SIZE];
   int pid;
   int stats;
 
   while(prom){
+    strcpy(path_cmd, "");
     write(1, "mysh> ", 6);
     read(0, cmd, BUF_SIZE);
-    if (strcmp(cmd, "exit\n") == 0)
-      break;
+    if (strncmp(cmd, "\n", 1) != 0) {
+      strcpy(&cmd[strlen(cmd) - 1], "\0");
+      if (strcmp(cmd, "exit") == 0)
+        break;
 
-    char *path = read_path(cmd);
+      strcpy(path_cmd, build_path(cmd));
 
-    pid = fork();
-    if (pid == -1) {
-      write(2, "failed to fork\n", 15);
-    } else if (pid == 0) {
-      execl("/bin/ls", "ls", NULL);
+      if (cmd_aru) {
+        args[0] = cmd;
+        args[1] = NULL;
+        pid = fork();
+        if (pid == -1) {
+          write(2, "failed to fork\n", 15);
+        } else if (pid == 0) {
+          execv(path_cmd, args);
+        }
+        waitpid(pid, &stats, 0);
+      } else {
+        printf("%s: Command Not Found\n", cmd);
+      }
     }
-    waitpid(pid, &stats, 0);
+
   }
 
   return 0;
@@ -44,53 +60,45 @@ int main(int argc, char const *argv[]) {
 // is_exist command
 //
 bool is_exist(char *path_cmd) {
-  bool aru;
   int fd;
-  struct stat info;
 
-  printf("path_cmd = %s", path_cmd);
-  printf("%lu\n", strlen(path_cmd));
+  cmd_aru = false;
   fd = open(path_cmd, O_RDONLY);
-  fstat(fd, &info);
-  if (info.st_mode & S_IFREG) {
-    aru = true;
-    printf("atta\n");
-  } else {
-    aru = false;
-    printf("nai\n");
+  if (fd >= 0) {
+    cmd_aru = true;
   }
   close(fd);
 
-  return aru;
+  return cmd_aru;
 }
 
 //
 // path + cmd
 //
-char *read_path(char *cmd) {
+char *build_path(char *cmd) {
   static char str[BUF_SIZE];
-  // char *path_cmd;
-  char path_cmd[BUF_SIZE];
+  static char path_cmd[BUF_SIZE];
   int f_fd;
   int i;
-  bool aru;
 
   f_fd = open(".path", O_RDONLY);
   read(f_fd, str, sizeof(str));
   close(f_fd);
 
+  strcpy(path_cmd, "");
+
   for (i = 0; i < strlen(str); ++i){
-    if (strncmp(&str[i], "\n", 1) == 0) {
+    if (strncmp(&str[i], ";", 1) == 0) {
       strcat(path_cmd, cmd);
-      strcat(path_cmd, "\0");
-      is_exist(path_cmd);
-      strcpy(path_cmd, "");
+      if (is_exist(path_cmd)) {
+        break;
+      } else {
+        strcpy(path_cmd, "");
+      }
     } else {
       strncat(path_cmd, &str[i], 1);
     }
   }
 
-  // aru = is_exist(path_cmd);
-
-  return str;
+  return path_cmd;
 }
