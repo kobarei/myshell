e036d56542824f341282fc8cb4103594c75d5401
kobarei <koba.reiji@gmail.com>
2014-01-11 12:57:06 +0900

    path + cmd

--- mysh.c
+++ mysh.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -9,74 +10,87 @@
 
 #define BUF_SIZE 256
 
-int prom = 1;
+bool prom = true;
 
-void left_prompt();
-void is_exist();
-void j_exit();
-char *read_path();
+bool is_exist(char *path_cmd);
+char *read_path(char *cmd);
 
 int main(int argc, char const *argv[]) {
-  char str[BUF_SIZE];
-  int i;
+  char cmd[BUF_SIZE];
   int pid;
   int stats;
 
-  char *path = read_path();
-
-  for (i = 0; i < strlen(path); ++i){
-    printf("%c", path[i]);
-    if (strncmp(&path[i], "\n", 1) == 0)
-      printf("改行");
-  }
-
   while(prom){
     write(1, "mysh> ", 6);
-    read(0, str, BUF_SIZE);
-    if (strcmp(str, "exit\n") == 0)
+    read(0, cmd, BUF_SIZE);
+    if (strcmp(cmd, "exit\n") == 0)
       break;
+
+    char *path = read_path(cmd);
 
     pid = fork();
     if (pid == -1) {
       write(2, "failed to fork\n", 15);
     } else if (pid == 0) {
-      execl("/bin/ls", "ls");
+      execl("/bin/ls", "ls", NULL);
     }
     waitpid(pid, &stats, 0);
   }
-  return 0;
-}
 
-//
-// output left prompt
-//
-void left_prompt() {
+  return 0;
 }
 
 //
 // is_exist command
 //
-void is_exist(char* str) {
+bool is_exist(char *path_cmd) {
+  bool aru;
   int fd;
+  struct stat info;
 
+  printf("path_cmd = %s", path_cmd);
+  printf("%lu\n", strlen(path_cmd));
+  fd = open(path_cmd, O_RDONLY);
+  fstat(fd, &info);
+  if (info.st_mode & S_IFREG) {
+    aru = true;
+    printf("atta\n");
+  } else {
+    aru = false;
+    printf("nai\n");
+  }
+  close(fd);
+
+  return aru;
 }
 
-char *read_path() {
-  static char str[256];
+//
+// path + cmd
+//
+char *read_path(char *cmd) {
+  static char str[BUF_SIZE];
+  // char *path_cmd;
+  char path_cmd[BUF_SIZE];
   int f_fd;
+  int i;
+  bool aru;
 
   f_fd = open(".path", O_RDONLY);
   read(f_fd, str, sizeof(str));
   close(f_fd);
-  strcat(str, "\n");
+
+  for (i = 0; i < strlen(str); ++i){
+    if (strncmp(&str[i], "\n", 1) == 0) {
+      strcat(path_cmd, cmd);
+      strcat(path_cmd, "\0");
+      is_exist(path_cmd);
+      strcpy(path_cmd, "");
+    } else {
+      strncat(path_cmd, &str[i], 1);
+    }
+  }
+
+  // aru = is_exist(path_cmd);
 
   return str;
-}
-
-//
-// EXIT if 'exit' input
-//
-void j_exit(char* str) {
-  if (strcmp(str, "exit\n") == 0)
-    prom = 0;
 }
